c9c29f39d20faeb3c5899ee2839115b156c26f0c
Eduardo José Tomey Esteban <95882356+Edu976@users.noreply.github.com>
2023-05-21 23:14:54 +0200

    Spawner
    Añadido un script para spawnear enemigos en un objeto vacio.

---
+++ Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class WaveSpawner : MonoBehaviour {
+
+    public List<GameObject> enemyPrefabs;  
+    public Transform spawnPoint;    
+    public float minSpawnInterval = 2f;   
+    public float maxSpawnInterval = 20f;  
+    public int enemyCount = 5;      
+
+    private void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+    private System.Collections.IEnumerator SpawnEnemies()
+    {
+        WaitForSeconds waitTime = new WaitForSeconds(maxSpawnInterval);
+
+        for (int i = 0; i < enemyCount; i++)
+        {
+            float spawnInterval = Random.Range(minSpawnInterval, maxSpawnInterval);
+            int randomIndex = Random.Range(0, enemyPrefabs.Count);
+            GameObject enemyPrefab = enemyPrefabs[randomIndex];
+            Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+            yield return new WaitForSeconds(spawnInterval);
+        }
+    }
+}
